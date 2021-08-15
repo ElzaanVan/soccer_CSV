6ddffe67e21b64e366979c0ed83011ca18c1e9f6
ElzaanVan <elzaanvanl@gmail.com>
2021-08-15 14:41:25 +0200

    Refactored code
    Made file reader more generic

--- build/index.js
+++ build/index.js
@@ -1,25 +1,29 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default.readFileSync('soccer.csv', {
-    //encoding type must be set to UTF-8 as to not get raw data, but a string
-    encoding: 'utf-8'
-})
-    .split('\n')
-    .map(function (row) {
-    return row.split(',');
-});
-console.log(matches);
+var CsvFileReader_1 = require("./CsvFileReader");
+var matches = new CsvFileReader_1.CsvFileReader("soccer.csv");
+matches.read();
+// const matchResults = {
+//     HomeWin: "H",
+//     AwayWin: "A",
+//     Draw: "D"
+// }
+// Use enum (enumeration)
+// Collection of closely related values
+// Fixed set and small set of values
+var matchResults;
+(function (matchResults) {
+    matchResults["HomeWin"] = "H";
+    matchResults["AwayWin"] = "A";
+    matchResults["Draw"] = "D";
+})(matchResults || (matchResults = {}));
 var manUnitedWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
-    if (match[1] === "Man United" && match[5] === "H") {
+for (var _i = 0, _a = matches.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === "Man United" && match[5] === matchResults.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === "A") {
+    else if (match[2] === "Man United" && match[5] === matchResults.AwayWin) {
         manUnitedWins++;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs.readFileSync('soccer.csv', {
-    //encoding type must be set to UTF-8 as to not get raw data, but a string
-    encoding: 'utf-8'
-})
-.split('\n')
-.map((row: string): string[] => {
-    return row.split(',');
-});
+const matches = new CsvFileReader("soccer.csv");
+matches.read();
 
-console.log(matches);
+// const matchResults = {
+//     HomeWin: "H",
+//     AwayWin: "A",
+//     Draw: "D"
+// }
+
+// Use enum (enumeration)
+// Collection of closely related values
+// Fixed set and small set of values
+enum matchResults {
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw = "D"
+}
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === "H") {
+for (let match of matches.data) {
+    if (match[1] === "Man United" && match[5] === matchResults.HomeWin) {
         manUnitedWins++;
-    } else if (match[2] === "Man United" && match[5] === "A") {
+    } else if (match[2] === "Man United" && match[5] === matchResults.AwayWin) {
         manUnitedWins++;
     }
 }
